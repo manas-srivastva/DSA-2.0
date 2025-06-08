4940915ca5d99fd69ec3549a772ab762b2094704
Manas Srivastava <srivastavamanas32@gmail.com>
2025-06-08 22:53:11 +0530

    Update program_lec_78_05.cpp

--- LinkedList/program_lec_78_05.cpp
+++ LinkedList/program_lec_78_05.cpp
@@ -11,17 +11,14 @@ class Node
         next =NULL;
     }
 };
-Node *createLinkedList (int arr[],int index,int size,Node*prev)
-{
+Node *createLinkedList(int arr[],int index,int size){
     // base case
-    if(index== size)
-    {
-        return prev;
-    }
+    if(index==size)
+    return NULL;
     Node *temp;
     temp=new Node(arr[index]);
-    temp->next=prev;
-   return createLinkedList(arr,index+1,size,temp);
+    temp->next=createLinkedList(arr,index+1,size);
+    return temp;
 }
 int main()
 {
@@ -30,7 +27,7 @@ int main()
  
 
   int arr[]={2,4,6,8,10};
-  Head=createLinkedList(arr,0,5,Head);// as head was intialized as null
+  Head=createLinkedList(arr,0,5);// as head was intialized as null
   // insert node at prticular posistion
   int x=3;// Insert posistion
   int value=30;
